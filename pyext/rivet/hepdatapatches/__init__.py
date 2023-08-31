ab333c2f272b37a639231110737c92c06028d388
Markus Seidel <markus.seidel@cern.ch>
2023-08-31 09:55:57 +0000

    CMS_2017_I1631985 (Z phi* at 8 TeV)

--- pyext/rivet/hepdatapatches/__init__.py
+++ pyext/rivet/hepdatapatches/__init__.py
@@ -53,6 +53,7 @@ from . import CLEOII_1999_I478217
 from . import CMD3_2016_I1385598
 from . import CMS_2016_I1487288
 from . import CMS_2017_I1608166
+from . import CMS_2017_I1631985
 from . import CRYSTAL_BALL_1988_I261078
 from . import CRYSTAL_BALL_1989_I263581
 from . import CRYSTAL_BALL_1991_I297905
