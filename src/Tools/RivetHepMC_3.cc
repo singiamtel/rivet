1b04adaeb853786d9e358abb01a8e589404ba6b0
Christian Gutschow <chris.g@cern.ch>
2024-07-06 21:06:36 +0000

    add support for HepMC 3.3.0

--- src/Tools/RivetHepMC_3.cc
+++ src/Tools/RivetHepMC_3.cc
@@ -1,7 +1,10 @@
 // -*- C++ -*-
 #include "Rivet/Config/DummyConfig.hh"
+#include "HepMC3/Version.h"
+#if HEPMC3_VERSION_CODE < 3003000
 #if HAVE_LIBZ
 #include "../Core/zstr/zstr.hpp"
+#endif
 #endif
 #include "HepMC3/GenCrossSection.h"
 #include "HepMC3/ReaderFactory.h"
