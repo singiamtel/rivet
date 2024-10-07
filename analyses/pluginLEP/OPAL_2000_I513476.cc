cda8d353f1079603714eb4964fbde11bda33938c
Christian Gutschow <chris.g@cern.ch>
2024-10-07 16:32:57 +0000

    OPAL_2000_I513476: make reentrant safe when mixing energies

--- analyses/pluginLEP/OPAL_2000_I513476.cc
+++ analyses/pluginLEP/OPAL_2000_I513476.cc
@@ -42,33 +42,34 @@ namespace Rivet {
 
       // Book histograms
       if (isCompatibleWithSqrtS(172*GeV)) {
-        ih = 1;
+        ih = 0;
       } else if (isCompatibleWithSqrtS(183*GeV)) {
-        ih = 2;
+        ih = 1;
       } else if (isCompatibleWithSqrtS(189*GeV)) {
-        ih = 3;
+        ih = 2;
       }
       else {
-        throw Error("Invalid CMS energy for OPAL_2000_I513476");
+        MSG_WARNING("Invalid CMS energy for OPAL_2000_I513476, assume 172 GeV.");
       }
-      book(_h_thrust    ,  1,1,ih);
-      book(_h_major     ,  2,1,ih);
-      book(_h_minor     ,  3,1,ih);
-      book(_h_aplanarity,  4,1,ih);
-      book(_h_oblateness,  5,1,ih);
-      book(_h_C         ,  6,1,ih);
-      book(_h_rhoH      ,  7,1,ih);
-      book(_h_sphericity,  8,1,ih);
-      book(_h_totalB    ,  9,1,ih);
-      book(_h_wideB     , 10,1,ih);
-      book(_h_y23       , 11,1,ih);
-      book(_h_mult      , 13,1,ih);
-      book(_h_pTin      , 15,1,ih);
-      book(_h_pTout     , 16,1,ih);
-      book(_h_y         , 17,1,ih);
-      book(_h_x         , 18,1,ih);
-      book(_h_xi        , 19,1,ih);
-      book(_sumW,"/TMP/sumW");
+      for (size_t i=0; i < _energies.size(); ++i) {
+        book(_h[i]["thrust"]    ,  1,1,i+1);
+        book(_h[i]["major"]     ,  2,1,i+1);
+        book(_h[i]["minor"]     ,  3,1,i+1);
+        book(_h[i]["aplanarity"],  4,1,i+1);
+        book(_h[i]["oblateness"],  5,1,i+1);
+        book(_h[i]["C"]         ,  6,1,i+1);
+        book(_h[i]["rhoH"]      ,  7,1,i+1);
+        book(_h[i]["sphericity"],  8,1,i+1);
+        book(_h[i]["totalB"]    ,  9,1,i+1);
+        book(_h[i]["wideB"]     , 10,1,i+1);
+        book(_h[i]["y23"]       , 11,1,i+1);
+        book(_h_mult[i]         , 13,1,i+1);
+        book(_h[i]["pTin"]      , 15,1,i+1);
+        book(_h[i]["pTout"]     , 16,1,i+1);
+        book(_h[i]["y"]         , 17,1,i+1);
+        book(_h[i]["x"]         , 18,1,i+1);
+        book(_h[i]["xi"]        , 19,1,i+1);
+      }
     }
 
 
@@ -78,8 +79,6 @@ namespace Rivet {
       const FinalState& cfs = apply<FinalState>(event, "CFS");
       if (cfs.size() < 2) vetoEvent;
 
-      _sumW->fill();
-
       // Get beams and average beam momentum
       const ParticlePair& beams = apply<Beam>(event, "Beams").beams();
       const double meanBeamMom = ( beams.first.p3().mod() +
@@ -87,102 +86,95 @@ namespace Rivet {
 
       // Thrust related
       const Thrust& thrust = apply<Thrust>(event, "Thrust");
-      _h_thrust    ->fill(thrust.thrust()     );
-      _h_major     ->fill(thrust.thrustMajor());
-      _h_minor     ->fill(thrust.thrustMinor());
-      _h_oblateness->fill(thrust.oblateness() );
+      _h[ih]["thrust"]    ->fill(thrust.thrust()     );
+      _h[ih]["major"]     ->fill(thrust.thrustMajor());
+      _h[ih]["minor"]     ->fill(thrust.thrustMinor());
+      _h[ih]["oblateness"]->fill(thrust.oblateness() );
 
       // Sphericity related
       const Sphericity& sphericity = apply<Sphericity>(event, "Sphericity");
-      _h_sphericity->fill(sphericity.sphericity());
-      _h_aplanarity->fill(sphericity.aplanarity());
+      _h[ih]["sphericity"]->fill(sphericity.sphericity());
+      _h[ih]["aplanarity"]->fill(sphericity.aplanarity());
 
       // C parameter
       const ParisiTensor& parisi = apply<ParisiTensor>(event, "Parisi");
-      _h_C->fill(parisi.C());
+      _h[ih]["C"]->fill(parisi.C());
 
       // Hemispheres
       const Hemispheres& hemi = apply<Hemispheres>(event, "Hemispheres");
 
-      _h_rhoH  ->fill(hemi.scaledMhigh());
-      _h_wideB ->fill(hemi.Bmax());
-      _h_totalB->fill(hemi.Bsum());
+      _h[ih]["rhoH"]  ->fill(hemi.scaledMhigh());
+      _h[ih]["wideB"] ->fill(hemi.Bmax());
+      _h[ih]["totalB"]->fill(hemi.Bsum());
 
       // Jets
       const FastJets& durjet = apply<FastJets>(event, "DurhamJets");
       const double y23 = durjet.clusterSeq()->exclusive_ymerge_max(2);
-      _h_y23->fill(y23);
+      _h[ih]["y23"]->fill(y23);
 
       // charged particles
-      _h_mult->fill(cfs.particles().size());
+      _h_mult[ih]->fill(cfs.particles().size());
       for (const Particle& p : cfs.particles()) {
         const Vector3 mom3  = p.p3();
         const double energy = p.E();
         const double pTinT  = dot(mom3, thrust.thrustMajorAxis());
         const double pToutT = dot(mom3, thrust.thrustMinorAxis());
-      	_h_pTin ->fill(fabs(pTinT/GeV) );
-      	_h_pTout->fill(fabs(pToutT/GeV));
+        _h[ih]["pTin"] ->fill(fabs(pTinT/GeV) );
+        _h[ih]["pTout"]->fill(fabs(pToutT/GeV));
         const double momT = dot(thrust.thrustAxis(), mom3);
         const double rapidityT = 0.5 * std::log((energy + momT) / (energy - momT));
-      	_h_y->fill(fabs(rapidityT));
+        _h[ih]["y"]->fill(fabs(rapidityT));
         const double mom = mom3.mod();
         const double scaledMom = mom/meanBeamMom;
         const double logInvScaledMom = -std::log(scaledMom);
-        _h_xi->fill(logInvScaledMom);
-        _h_x ->fill(scaledMom      );
+        _h[ih]["xi"]->fill(logInvScaledMom);
+        _h[ih]["x"] ->fill(scaledMom      );
       }
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-      scale(_h_thrust    ,1./ *_sumW);
-      scale(_h_major     ,1./ *_sumW);
-      scale(_h_minor     ,1./ *_sumW);
-      scale(_h_aplanarity,1./ *_sumW);
-      scale(_h_oblateness,1./ *_sumW);
-      scale(_h_C         ,1./ *_sumW);
-      scale(_h_rhoH      ,1./ *_sumW);
-      scale(_h_sphericity,1./ *_sumW);
-      scale(_h_totalB    ,1./ *_sumW);
-      scale(_h_wideB     ,1./ *_sumW);
-      scale(_h_y23       ,1./ *_sumW);
-      scale(_h_mult      ,100./ *_sumW);
-      scale(_h_pTin      ,1./ *_sumW);
-      scale(_h_pTout     ,1./ *_sumW);
-      scale(_h_y         ,1./ *_sumW);
-      scale(_h_x         ,1./ *_sumW);
-      scale(_h_xi        ,1./ *_sumW);
       // mean multiplicity
-      const double nch     = _h_mult->xMean();
-      const double nch_err = _h_mult->xStdErr();
       BinnedEstimatePtr<int> m_ch;
       book(m_ch,14,1,1);
-      m_ch->bin(ih).set(nch, nch_err);
       // mean ptIn
-      double pTin     = _h_pTin->xMean();
-      double pTin_err = _h_pTin->xStdErr();
       BinnedEstimatePtr<int> m_pTin;
       book(m_pTin,20,1,1);
-      m_pTin->bin(ih).set(pTin, pTin_err);
       // mean ptOut
-      double pTout     = _h_pTout->xMean();
-      double pTout_err = _h_pTout->xStdErr();
       BinnedEstimatePtr<int> m_pTout;
       book(m_pTout,20,1,2);
-      m_pTout->bin(ih).set(pTout, pTout_err);
       // mean y
-      double y     = _h_y->xMean();
-      double y_err = _h_y->xStdErr();
       BinnedEstimatePtr<int> m_y;
       book(m_y,20,1,3);
-      m_y->bin(ih).set(y, y_err);
       // mean x
-      double x     = _h_x->xMean();
-      double x_err = _h_x->xStdErr();
       BinnedEstimatePtr<int> m_x;
       book(m_x,20,1,4);
-      m_x->bin(ih).set(x, x_err);
+
+      // scale histos + fill averages
+      for (size_t i=0; i<_energies.size(); ++i) {
+        const double sumw = _h_mult[i]->sumW();
+        if (sumw == 0)  continue;
+
+        scale(_h[i], 1./sumw);
+        scale(_h_mult[i], 100./sumw);
+
+        const double nch     = _h_mult[i]->xMean();
+        const double nch_err = _h_mult[i]->xStdErr();
+        m_ch->bin(i+1).set(nch, nch_err);
+        double pTin     = _h[i]["pTin"]->xMean();
+        double pTin_err = _h[i]["pTin"]->xStdErr();
+        m_pTin->bin(i+1).set(pTin, pTin_err);
+        double pTout     = _h[i]["pTout"]->xMean();
+        double pTout_err = _h[i]["pTout"]->xStdErr();
+        m_pTout->bin(i+1).set(pTout, pTout_err);
+        double y     = _h[i]["y"]->xMean();
+        double y_err = _h[i]["y"]->xStdErr();
+        m_y->bin(i+1).set(y, y_err);
+        double x     = _h[i]["x"]->xMean();
+        double x_err = _h[i]["x"]->xStdErr();
+        m_x->bin(i+1).set(x, x_err);
+      }
     }
 
     /// @}
@@ -190,11 +182,10 @@ namespace Rivet {
 
     /// @name Histograms
     /// @{
-    Histo1DPtr _h_thrust,_h_major,_h_minor,_h_aplanarity,_h_oblateness,_h_C,_h_rhoH,_h_sphericity;
-    Histo1DPtr _h_totalB,_h_wideB,_h_y23,_h_pTin,_h_pTout,_h_y,_h_x,_h_xi;
-    BinnedHistoPtr<int> _h_mult;
-    CounterPtr _sumW;
-    size_t ih = -1;
+    map<string,Histo1DPtr> _h[3];
+    BinnedHistoPtr<int> _h_mult[3];
+    size_t ih = 0;
+    const vector<double> _energies{ 172., 183., 189. };
     /// @}
 
 
