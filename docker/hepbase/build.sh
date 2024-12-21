a0cf483073667482ab421823d6fe0d7a2cf43940
Andy Buckley <andy.buckley@cern.ch>
2024-12-21 12:54:25 +0000

    Actually pass in hepbase BASEARGS!

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -7,10 +7,6 @@ HEPMC_VERSION=3.3.0
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../docker-common.sh"
 
-BASEARGS="--build-arg RIVETBS_VERSION=$RIVETBS_VERSION"
-BASEARGS+=" --build-arg LHAPDF_VERSION=$LHAPDF_VERSION"
-BASEARGS+=" --build-arg HEPMC_VERSION=3.3.0"
-
 if [[ -z "$OS" || -x "$TOOLS" ]]; then
     echo "You need to set OS=ubuntu|fedora and TOOLS=gcc|llvm variables"
     exit 1
@@ -18,11 +14,15 @@ fi
 
 PKG=hepstore/hepbase-$OS-$TOOLS
 
+BASEARGS="--build-arg RIVETBS_VERSION=$RIVETBS_VERSION"
+BASEARGS+=" --build-arg LHAPDF_VERSION=$LHAPDF_VERSION"
+BASEARGS+=" --build-arg HEPMC_VERSION=$HEPMC_VERSION"
+
 echo "Building $PKG image"
-dx_build $BUILDFLAGS -f Dockerfile.$OS --build-arg LATEX=0 --build-arg BUILD_TOOLS=$TOOLS -t $PKG:latest
+dx_build $BUILDFLAGS -f Dockerfile.$OS $BASEARGS --build-arg LATEX=0 --build-arg BUILD_TOOLS=$TOOLS -t $PKG:latest
 test "$PUSH" = 1 && xdocker push $PKG && sleep $SLEEP
 
 PKG+="-latex"
 echo "Building $PKG image"
-dx_build $BUILDFLAGS -f Dockerfile.$OS --build-arg LATEX=1 --build-arg BUILD_TOOLS=$TOOLS -t $PKG:latest
+dx_build $BUILDFLAGS -f Dockerfile.$OS $BASEARGS --build-arg LATEX=1 --build-arg BUILD_TOOLS=$TOOLS -t $PKG:latest
 test "$PUSH" = 1 && xdocker push $PKG && sleep $SLEEP
