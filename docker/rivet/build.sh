5c9c77a619ede881dda7b5423d1e3f2e9d8437a5
Andy Buckley <andy.buckley@cern.ch>
2025-01-03 00:41:35 +0000

    Simplify Rivet Docker build script

--- docker/rivet/build.sh
+++ docker/rivet/build.sh
@@ -6,33 +6,28 @@ RIVET_BRANCHES=${RIVET_BRANCHES:-rivet-4.0.2}
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../docker-common.sh"
 
-BUILDFLAGS=" --build-arg YODA_BRANCH=$YODA_BRANCH"
+BUILDFLAGS+=" --build-arg YODA_BRANCH=$YODA_BRANCH"
+BUILDFLAGS+=" --build-arg ARCH=ubuntu-gcc-hepmc3-py3"
+
 PKG="hepstore/rivet"
+TAGS="${RIVET_VERSION}"
 
 if [[ "$MAIN" = 1 ]]; then RIVET_BRANCHES="main $RIVET_BRANCHES"; fi
 
-# IMPORTANT: put 'latest' version last in the list: it gets reused after the loop
 for RIVET_BRANCH in $RIVET_BRANCHES; do
     RIVET_VERSION=${RIVET_BRANCH#rivet-}
 
-    MSG="Building Rivet $RIVET_VERSION image with architecture ="
-    for CC in gcc; do  # clang  #< last architecture in the list will also get the no-arch name
-        ARCH=ubuntu-$CC-hepmc3-py3
-        echo "@@ Building Rivet $RIVET_VERSION image with architecture = $ARCH"
-        TAGS="$PKG:$RIVET_VERSION-$ARCH $PKG:$RIVET_VERSION"
-        for t in $TAGS; do TAGFLAGS+=" -t $t"; done
-        dx_build $BUILDFLAGS --build-arg RIVET_BRANCH=$RIVET_BRANCH --build-arg ARCH=$ARCH $TAGFLAGS
-        if [[ "$PUSH" = 1 ]]; then
-            for tag in $TAGS; do xdocker push $tag; done
-            sleep ${SLEEP:-1}
-        fi
-        echo -e "\n\n\n"
-    done
+    echo "@@ Building Rivet $RIVET_VERSION image"
+    dx_build $BUILDFLAGS --build-arg RIVET_BRANCH=$RIVET_BRANCH $TAGFLAGS
+    if [[ "$PUSH" = 1 ]]; then
+        for tag in $TAGS; do xdocker push $PKG:$tag; done
+    fi
+    echo -e "\n\n\n"
 done
 
-if [[ "$LATEST" = 1 ]]; then
-    dx_build $BUILDFLAGS --build-arg RIVET_BRANCH=$RIVET_BRANCH --build-arg ARCH=$ARCH $TAGFLAGS -t $PKG:latest
-    if [[ "$PUSH" = 1 ]]; then
-        xdocker push hepstore/rivet:latest
-    fi
-fi
+# if [[ "$LATEST" = 1 ]]; then
+#     dx_build $BUILDFLAGS --build-arg RIVET_BRANCH=$RIVET_BRANCH --build-arg ARCH=$ARCH $TAGFLAGS -t $PKG:latest
+#     if [[ "$PUSH" = 1 ]]; then
+#         xdocker push hepstore/rivet:latest
+#     fi
+# fi
