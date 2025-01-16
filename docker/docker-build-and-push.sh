29ff1cc0483fd6b6a050eb8a632584230f3a33a9
Andy Buckley <andy.buckley@cern.ch>
2025-01-16 00:31:08 +0000

    Remove Docker configs -- in new hepstore package

--- docker/docker-build-and-push.sh
+++
@@ -1,8 +0,0 @@
-test "$LATEST" = 1 && TAGS+=" latest"
-for tag in $TAGS; do TAGFLAGS+=" -t $PKG:$tag"; done
-
-dx_build $BUILDFLAGS $TAGFLAGS
-
-if [[ "$PUSH" = 1 ]]; then
-    for tag in $TAGS; do xdocker push $PKG:$tag; done
-fi
--- docker/hepbase-latex/TODO.md
+++
@@ -1,3 +0,0 @@
-- Add LHAPDF
-
-- Add some versioning
