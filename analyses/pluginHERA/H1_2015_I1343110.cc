5a4f813c2320954f1d357625cc8c9c456d6ff6b7
Peter Meinzinger <peter.meinzinger@uzh.ch>
2025-02-25 12:25:04 +0000

    Fix HERA diffractive jets analyses

--- analyses/pluginHERA/H1_2015_I1343110.cc
+++ analyses/pluginHERA/H1_2015_I1343110.cc
@@ -9,289 +9,6 @@
 
 namespace Rivet {
 
-
-  namespace H1_2015_I1343110_PROJECTIONS {
-
-    /// Projection to find the largest gaps and the masses of the two
-    /// systems separated by the gap. Based on the HZTools gap-finding
-    /// method (hzhadgap.F). Note that gaps are found in the HCM frame.
-    ///
-    /// @author Christine O. Rasmussen.
-    class RapidityGap : public Projection {
-    public:
-
-      /// Type of DIS boost to apply
-      enum Frame { HCM, LAB, XCM };
-
-      RapidityGap() {
-        setName("RapidityGap");
-        declare(DISKinematics(), "DISKIN");
-        declare(DISFinalState(DISFrame::HCM), "DISFS");
-      }
-
-      RIVET_DEFAULT_PROJ_CLONE(RapidityGap);
-
-      /// Import to avoid warnings about overload-hiding
-      using Projection::operator =;
-
-      double M2X() const { return _M2X; }
-      double M2Y() const { return _M2Y; }
-      double t() const { return _t; }
-      double gap() const { return _gap; }
-      double gapUpp() const { return _gapUpp; }
-      double gapLow() const { return _gapLow; }
-      double EpPzX(Frame f) const {
-        if (f == LAB) return _ePpzX_LAB;
-        else if (f == XCM) return _ePpzX_XCM;
-        else return _ePpzX_HCM;
-      }
-      double EmPzX(Frame f) const {
-        if (f == LAB) return _eMpzX_LAB;
-        else if (f == XCM) return _eMpzX_XCM;
-        else return _eMpzX_HCM;
-      }
-      FourMomentum pX(Frame f) const {
-        if (f == LAB) return _momX_LAB;
-        else if (f == XCM) return _momX_XCM;
-        else return _momX_HCM;
-      }
-      FourMomentum pY(Frame f) const {
-        if (f == LAB) return _momY_LAB;
-        else if (f == XCM) return _momY_XCM;
-        else return _momY_HCM;
-      }
-      const Particles& systemX(Frame f) const {
-        if (f == LAB) return _pX_LAB;
-        else if (f == XCM) return _pX_XCM;
-        else return _pX_HCM;
-      }
-      const Particles& systemY(Frame f) const {
-        if (f == LAB) return _pY_LAB;
-        else if (f == XCM) return _pY_XCM;
-        else return _pY_HCM;
-      }
-
-    protected:
-
-      virtual CmpState compare(const Projection& p) const {
-        const RapidityGap& other = pcast<RapidityGap>(p);
-        return mkNamedPCmp(other, "DISKIN") || mkNamedPCmp(other, "DISFS");
-      }
-
-      virtual void project(const Event& e){
-        const DISKinematics& dk = apply<DISKinematics>(e, "DISKIN");
-        const Particles& p      = apply<DISFinalState>(e, "DISFS").particles(cmpMomByEta);
-        findgap(p, dk);
-      }
-
-      void clearAll(){
-        _M2X = _M2Y = _t = _gap = 0.;
-        _gapUpp = _gapLow = -8.;
-        _ePpzX_HCM = _eMpzX_HCM =_ePpzX_LAB = _eMpzX_LAB = _ePpzX_XCM = _eMpzX_XCM = 0.;
-        _momX_HCM.setPE(0., 0., 0., 0.);
-        _momY_HCM.setPE(0., 0., 0., 0.);
-        _momX_XCM.setPE(0., 0., 0., 0.);
-        _momY_XCM.setPE(0., 0., 0., 0.);
-        _momX_LAB.setPE(0., 0., 0., 0.);
-        _momY_LAB.setPE(0., 0., 0., 0.);
-        _pX_HCM.clear();
-        _pY_HCM.clear();
-        _pX_XCM.clear();
-        _pY_XCM.clear();
-        _pX_LAB.clear();
-        _pY_LAB.clear();
-      }
-
-      void findgap(const Particles& particles, const DISKinematics& diskin){
-
-        clearAll();
-
-        // Begin by finding largest gap and gapedges between all final
-        // state particles in HCM frame.
-        int nP  = particles.size();
-        int dir = diskin.orientation();
-        for (int i = 0; i < nP-1; ++i){
-          double tmpGap = abs(particles[i+1].eta() - particles[i].eta());
-          if (tmpGap > _gap) {
-            _gap    = tmpGap;
-            _gapLow = (dir > 0) ? particles[i].eta() : dir * particles[i+1].eta();
-            _gapUpp = (dir > 0) ? particles[i+1].eta() : dir * particles[i].eta();
-          }
-        }
-
-        // Define the two systems X and Y.
-        Particles tmp_pX, tmp_pY;
-        for (const Particle& ip : particles) {
-          if (dir * ip.eta() > _gapLow) tmp_pX.push_back(ip);
-          else tmp_pY.push_back(ip);
-        }
-
-        Particles pX, pY;
-        pX = (dir < 0) ? tmp_pY : tmp_pX;
-        pY = (dir < 0) ? tmp_pX : tmp_pY;
-
-        // Find variables related to HCM frame.
-        // Note that HCM has photon along +z, as opposed to
-        // H1 where proton is along +z. This results in a sign change
-        // as compared to H1 papers!
-
-        // X - side
-        FourMomentum momX;
-        for (const Particle& jp : pX) {
-          momX  += jp.momentum();
-          _ePpzX_HCM += jp.E() - jp.pz(); // Sign + => -
-          _eMpzX_HCM += jp.E() + jp.pz(); // Sign - => +
-        }
-        _momX_HCM = momX;
-        _pX_HCM   = pX;
-        _M2X      = _momX_HCM.mass2();
-
-        // Y - side
-        FourMomentum momY;
-        for (const Particle& kp : pY) momY += kp.momentum();
-        _momY_HCM = momY;
-        _pY_HCM   = pY;
-        _M2Y      = _momY_HCM.mass2();
-
-        // Find variables related to LAB frame
-        const LorentzTransform hcmboost   = diskin.boostHCM();
-        const LorentzTransform hcminverse = hcmboost.inverse();
-        _momX_LAB = hcminverse.transform(_momX_HCM);
-        _momY_LAB = hcminverse.transform(_momY_HCM);
-
-        // Find momenta in XCM frame. Note that it is HCM frame that is
-        // boosted, resulting in a sign change later!
-        const bool doXCM = (momX.betaVec().mod2() < 1.);
-        if (doXCM) {
-          const LorentzTransform xcmboost =
-            LorentzTransform::mkFrameTransformFromBeta(momX.betaVec());
-          _momX_XCM = xcmboost.transform(momX);
-          _momY_XCM = xcmboost.transform(momY);
-        }
-
-        for (const Particle& jp : pX) {
-          // Boost from HCM to LAB.
-          FourMomentum lab = hcminverse.transform(jp.momentum());
-          _ePpzX_LAB += lab.E() + dir * lab.pz();
-          _eMpzX_LAB += lab.E() - dir * lab.pz();
-          Particle plab = jp;
-          plab.setMomentum(lab);
-          _pX_LAB.push_back(plab);
-          // Set XCM. Note that since HCM frame is boosted to XCM frame,
-          // we have a sign change
-          if (doXCM) {
-            const LorentzTransform xcmboost =
-              LorentzTransform::mkFrameTransformFromBeta(_momX_HCM.betaVec());
-            FourMomentum xcm = xcmboost.transform(jp.momentum());
-            _ePpzX_XCM += xcm.E() - xcm.pz(); // Sign + => -
-            _eMpzX_XCM += xcm.E() + xcm.pz(); // Sign - => +
-            Particle pxcm = jp;
-            pxcm.setMomentum(xcm);
-            _pX_XCM.push_back(pxcm);
-          }
-        }
-
-        for (const Particle& jp : pY) {
-          // Boost from HCM to LAB
-          FourMomentum lab = hcminverse.transform(jp.momentum());
-          Particle plab = jp;
-          plab.setMomentum(lab);
-          _pY_LAB.push_back(plab);
-          // Boost from HCM to XCM
-          if (doXCM) {
-            const LorentzTransform xcmboost =
-              LorentzTransform::mkFrameTransformFromBeta(_momX_HCM.betaVec());
-            FourMomentum xcm = xcmboost.transform(jp.momentum());
-            Particle pxcm = jp;
-            pxcm.setMomentum(xcm);
-            _pY_XCM.push_back(pxcm);
-          }
-        }
-
-        // Boost p from LAB to HCM frame to find t.
-        ///
-        /// @note Currently can only handle gap on proton side.
-        ///
-        /// @todo Expand to also handle gap on photon side
-        const FourMomentum proton = hcmboost.transform(diskin.beamHadron().momentum());
-        FourMomentum pPom         = proton - _momY_HCM;
-        _t                        = pPom * pPom;
-
-      }
-
-    private:
-
-      double _M2X, _M2Y, _t;
-      double _gap, _gapUpp, _gapLow;
-      double _ePpzX_LAB, _eMpzX_LAB, _ePpzX_HCM, _eMpzX_HCM, _ePpzX_XCM, _eMpzX_XCM;
-      FourMomentum _momX_HCM, _momY_HCM,_momX_LAB, _momY_LAB, _momX_XCM, _momY_XCM;
-      Particles _pX_HCM, _pY_HCM, _pX_LAB, _pY_LAB, _pX_XCM, _pY_XCM;
-
-    };
-
-    /// Projection to boost system X (photon+Pomeron) particles into its rest frame.
-    ///
-    /// @author Ilkka Helenius
-    class BoostedXSystem : public FinalState {
-    public:
-
-      BoostedXSystem(const FinalState& fs) {
-        setName("BoostedXSystem");
-        declare(fs,"FS");
-        declare(RapidityGap(), "RAPGAP");
-      }
-
-      // Return the boost to XCM frame.
-      const LorentzTransform& boost() const { return _boost; }
-
-      RIVET_DEFAULT_PROJ_CLONE(BoostedXSystem);
-
-      /// Import to avoid warnings about overload-hiding
-      using Projection::operator =;
-
-    protected:
-
-      // Apply the projection on the supplied event.
-      void project(const Event& e){
-
-        const RapidityGap& rg = apply<RapidityGap>(e, "RAPGAP");
-
-        // Total momentum of the system X.
-        const FourMomentum pX = rg.pX(RapidityGap::HCM);
-
-        // Reset the boost. Is there a separate method for this?
-        _boost = combine(_boost, _boost.inverse());
-
-        // Define boost only when numerically safe, otherwise negligible.
-        if (pX.betaVec().mod2() < 1.)
-          _boost = LorentzTransform::mkFrameTransformFromBeta(pX.betaVec());
-
-        // Boost the particles from system X.
-        _theParticles.clear();
-        _theParticles.reserve(rg.systemX(RapidityGap::HCM).size());
-        for (const Particle& p : rg.systemX(RapidityGap::HCM)) {
-          Particle temp = p;
-          temp.setMomentum(_boost.transform(temp.momentum()));
-          _theParticles.push_back(temp);
-        }
-
-      }
-
-      // Compare projections.
-      CmpState compare(const Projection& p) const {
-        const BoostedXSystem& other = pcast<BoostedXSystem>(p);
-        return mkNamedPCmp(other, "RAPGAP") || mkNamedPCmp(other, "FS");
-      }
-
-    private:
-
-      LorentzTransform _boost;
-
-    };
-
-  }
-
-
   /// @brief H1 diffractive dijets
   ///
   /// Diffractive dijets H1 with 920 GeV p and 27.5 GeV e
@@ -304,9 +21,6 @@ namespace Rivet {
     /// Constructor
     RIVET_DEFAULT_ANALYSIS_CTOR(H1_2015_I1343110);
 
-    typedef H1_2015_I1343110_PROJECTIONS::RapidityGap RapidityGap;
-    typedef H1_2015_I1343110_PROJECTIONS::BoostedXSystem BoostedXSystem;
-
     /// @name Analysis methods
     /// @{
 
@@ -314,12 +28,11 @@ namespace Rivet {
     void init() {
 
       declare(DISKinematics(), "Kinematics");
-      const DISFinalState& disfs = declare(DISFinalState(DISFrame::HCM), "DISFS");
-      const BoostedXSystem& disfsXcm = declare( BoostedXSystem(disfs), "BoostedXFS");
-      declare(FastJets(disfsXcm, fastjet::JetAlgorithm::kt_algorithm, fastjet::RecombinationScheme::pt_scheme, 1.0,
-                       JetMuons::ALL, JetInvisibles::NONE, nullptr), "DISFSJets");
+      declare(DISFinalState(DISFrame::LAB), "FSLab");
+      declare(FastJets(DISFinalState(DISFrame::HCM),
+        fastjet::JetAlgorithm::kt_algorithm, fastjet::RecombinationScheme::pt_scheme, 1.0,
+        JetMuons::ALL, JetInvisibles::NONE, nullptr), "DISFSJets");
       declare(DISDiffHadron(), "Hadron");
-      declare(RapidityGap(), "RapidityGap");
 
       // Book histograms from REF data
       book(_h_PHO_sig_sqrts, 1, 1, 1);
@@ -350,6 +63,7 @@ namespace Rivet {
       book(_h_PHODIS_Etj1, 23, 1, 1);
 
       isPHO  = false;
+      nVeto0 = 0;
       nVeto1 = 0;
       nVeto2 = 0;
       nVeto3 = 0;
@@ -367,9 +81,8 @@ namespace Rivet {
       isPHO  = false;
 
       // Projections - special handling of events where no proton found:
-      const RapidityGap&    rg = apply<RapidityGap>(event, "RapidityGap");
       const DISKinematics& kin = apply<DISKinematics>(event, "Kinematics");
-      const BoostedXSystem& disfsXcm = apply<BoostedXSystem>( event, "BoostedXFS");
+      if (kin.failed()) vetoEvent;
       Particle hadronOut;
       Particle hadronIn;
       try {
@@ -379,6 +92,7 @@ namespace Rivet {
       } catch (const Error& e){
         vetoEvent;
       }
+      ++nVeto0;
 
       // Determine kinematics: H1 has +z = proton direction
       int dir   = kin.orientation();
@@ -397,92 +111,66 @@ namespace Rivet {
 
       // Find diffractive variables as defined in paper.
       // Note tagged protons in VFPS => smaller allowed xPom range
-      // xPom = 1 - E'/E, M2X from hadrons, t = (P-P')^2
-      const double M2X  = rg.M2X();
-      const double abst = abs(rg.t());
+      // xPom = 1 - E'/E, t = (P-P')^2
+      const double abst = abs((hadronIn.mom() - hadronOut.mom()).mass2());
       const double xPom = 1. - hadronOut.energy() / hadronIn.energy();
-
-      //cout << "\nhadout=" << hadronOut.energy() << ", hadin=" << hadronIn.energy() << endl;
-      //cout << "xPomH1=" << (Q2+M2X) / (y * sqr(sqrtS())) << endl;
-      //cout << "|t|=" << abst << ", xPom=" << xPom << endl;
       // Veto if outside allowed region
-      if (abst > 0.6 * GeV2)              vetoEvent;
+      if (abst > 0.6 * GeV2) vetoEvent;
       ++nVeto2;
       if (!inRange(xPom, 0.010, 0.024)) vetoEvent;
       ++nVeto3;
 
-      // Jet selection. Note jets are found in XCM frame, but
+      // Jet selection. Note jets are found in HCM frame, but
       // eta cut is applied in lab frame!
-      Cut jetcuts = Cuts::Et > 4.* GeV;
-      Jets jets   = apply<FastJets>(event, "DISFSJets").jets(jetcuts, cmpMomByEt);
+      Jets jets   = apply<FastJets>(event, "DISFSJets").jets(Cuts::Et > 4.* GeV, cmpMomByEt);
       // Veto if not dijets and if Et_j1 < 5.5
       if (jets.size() < 2)          vetoEvent;
       if (jets[0].Et() < 5.5 * GeV) vetoEvent;
       ++nVeto4;
-      // Find Et_jet1 in XCM frame
-      double EtJet1 = jets[0].Et() * GeV;
 
-      //cout << "gamma*p frame:" << endl;
-      //cout << "Et1=" << jets[0].Et() << ", E1=" << jets[0].E() << ", pz1=" << jets[0].pz()  << ", m1=" << jets[0].mass() << endl;
-      //cout << "Et2=" << jets[1].Et() << ", E2=" << jets[1].E() << ", pz2=" << jets[1].pz()  << ", m2=" << jets[1].mass() << endl;
+      const double EtJet1 = jets[0].Et();
 
-      // Transform from XCM to HCM
-      const LorentzTransform xcmboost = disfsXcm.boost();
-      for (int i = 0; i < 2; ++i) jets[i].transformBy(xcmboost.inverse());
-
-      // Find mass of jets and EpPz, EmPz of jets in HCM frame.
-      FourMomentum momJets = jets[0].momentum() + jets[1].momentum();
-      double M2jets        = momJets.mass2();
-      double EpPzJets      = 0.;
-      double EmPzJets      = 0.;
-      // Note sign change wrt. H1 because photon is in +z direction
-      for (int i = 0; i < 2; ++i){
-        EpPzJets += jets[i].E() - jets[i].pz(); // Sign: + => -
-        EmPzJets += jets[i].E() + jets[i].pz(); // Sign: - => +
+      // Transform from HCM to LAB and apply eta cut
+      const LorentzTransform boost = kin.boostHCM();
+      for (int i = 0; i < 2; ++i) {
+        jets[i].transformBy(boost.inverse());
+        if (!inRange(dir * jets[i].eta(),-1,2.5)) vetoEvent;
       }
-
-      // Transform the jets from HCM to LAB frame where eta cut is
-      // applied for photoproduction.
-      const LorentzTransform hcmboost = kin.boostHCM();
-      for (int i = 0; i < 2; ++i) jets[i].transformBy(hcmboost.inverse());
-      double etaLabJet1 = dir * jets[0].eta();
-      double etaLabJet2 = dir * jets[1].eta();
-      if (!inRange(etaLabJet1, -1., 2.5)) vetoEvent;
-      if (!inRange(etaLabJet2, -1., 2.5)) vetoEvent;
       ++nVeto5;
+
+      double sumE(0.), sumPz(0.);
+      FourMomentum tot_mom;
+      for(const Particle & part : apply<FinalState>(event,"FSLab").particles()) {
+        if (part.genParticle() != hadronOut.genParticle()) {
+          sumE += part.energy();
+          sumPz += part.pz();
+          tot_mom += part.mom();
+        }
+      }
+      double EplusPz = sumE + dir * sumPz;
+      double EminusPz = sumE - dir * sumPz;
+      double M2X = tot_mom.mass2();
 
       // Pseudorapidity distributions are examined in lab frame:
       double deltaEtaJets = abs(dir * jets[0].eta() - dir * jets[1].eta());
       double avgEtaJets   = 0.5 * (dir * jets[0].eta() + dir * jets[1].eta());
 
       // Evaluate observables
-      double zPomJets, xGamJets = 0.;
+      double zPomJets(0.), xGamJets(0.);
+      FourMomentum pj = jets[0].momentum()+jets[1].momentum();
       if (isPHO){
-        zPomJets = EpPzJets / rg.EpPzX(RapidityGap::HCM);
-        xGamJets = EmPzJets / rg.EmPzX(RapidityGap::HCM);
-        //cout << "xGamJets=" << xGamJets << endl;
+        zPomJets = (pj.E() + dir * pj.pz()) / EplusPz;
+        xGamJets = (pj.E() - dir * pj.pz()) / EminusPz;
       } else {
-        zPomJets = (Q2 + M2jets) / (Q2 + M2X);
+        zPomJets = (Q2 + pj.mass2()) / (Q2 + M2X);
       }
-
-      //cout << "lab frame:" << endl;
-      //cout << "Et1=" << jets[0].Et() << ", E1=" << jets[0].E() << ", pz1=" << jets[0].pz() << ", m1=" << jets[0].mass() <<  endl;
-      //cout << "Et2=" << jets[1].Et() << ", E2=" << jets[1].E() << ", pz2=" << jets[1].pz() << ", m2=" << jets[1].mass() <<  endl;
-      //cout << "EpPzJets=" << EpPzJets << ", EmPzJets=" << EmPzJets << endl;
-      //cout << "Et*exp(eta)=" << jets[0].Et()*exp(etaLabJet1) + jets[1].Et()*exp(etaLabJet2) << endl;
-      //cout << "Et*exp(-eta)=" << jets[0].Et()*exp(-etaLabJet1) + jets[1].Et()*exp(-etaLabJet2) << endl;
-      //cout << "EpPz=" << rg.EpPzX(RapidityGap::HCM) << ", EmPz=" << rg.EmPzX(RapidityGap::HCM) << endl;
-      //cout << "2 xPom Ep=" << 2. * xPom * kin.beamHadron().E() << ", 2 y Ee=" << 2. * y * kin.beamLepton().E() << endl;
-      //cout << "xGam=" << xGamJets << ", zPom=" << zPomJets << endl;
-      //cout << "M12=" << M2jets << ", deltaEta=" << deltaEtaJets << ", avgEta=" << avgEtaJets << endl;
-
       // Veto events with zPom > 0.8
       if (zPomJets > 0.8) vetoEvent;
       ++nVeto6;
 
       // Now fill histograms
       if (isPHO){
-        _h_PHO_sig_sqrts     ->fill(319);
+        _h_PHO_sig_sqrts     ->fill(sqrtS()/GeV);
         _h_PHO_dsigdz        ->fill(zPomJets);
         _h_PHO_dsigdxPom     ->fill(xPom);
         _h_PHO_dsigdy        ->fill(y);
@@ -492,7 +180,7 @@ namespace Rivet {
         _h_PHO_dsigdDeltaEta ->fill(deltaEtaJets);
         _h_PHO_dsigdAvgEta   ->fill(avgEtaJets);
       } else {
-        _h_DIS_sig_sqrts     ->fill(319);
+        _h_DIS_sig_sqrts     ->fill(sqrtS()/GeV);
         _h_DIS_dsigdz        ->fill(zPomJets);
         _h_DIS_dsigdxPom     ->fill(xPom);
         _h_DIS_dsigdy        ->fill(y);
@@ -543,11 +231,12 @@ namespace Rivet {
       if (_h_DIS_dsigdEtj1->numEntries() != 0)
         divide(_h_PHO_dsigdEtj1, _h_DIS_dsigdEtj1, _h_PHODIS_Etj1);
 
-      const double dPHO = nPHO;
+      const double dPHO = numEvents();
       MSG_INFO("H1_2015_I1343110");
       MSG_INFO("Cross section = " << crossSection()/picobarn << " pb");
       MSG_INFO("Number of events = " << numEvents() << ", sumW = " << sumOfWeights());
       MSG_INFO("Number of PHO = " << nPHO << ", number of DIS = " << nDIS);
+      MSG_INFO("Events passing event setup     = " << nVeto0 << " (" << nVeto0/dPHO * 100. << "%)" );
       MSG_INFO("Events passing electron veto   = " << nVeto1 << " (" << nVeto1/dPHO * 100. << "%)" );
       MSG_INFO("Events passing t veto          = " << nVeto2 << " (" << nVeto2/dPHO * 100. << "%)" );
       MSG_INFO("Events passing xPom            = " << nVeto3 << " (" << nVeto3/dPHO * 100. << "%)" );
@@ -594,7 +283,7 @@ namespace Rivet {
     /// @}
 
     bool isPHO;
-    int  nVeto1, nVeto2, nVeto3, nVeto4, nVeto5, nVeto6;
+    int  nVeto0, nVeto1, nVeto2, nVeto3, nVeto4, nVeto5, nVeto6;
     int  nPHO, nDIS;
   };
 
--- analyses/pluginHERA/H1_2015_I1343110.plot
+++ analyses/pluginHERA/H1_2015_I1343110.plot
@@ -1,7 +1,7 @@
 # BEGIN PLOT /H1_2015_I1343110/d01-x01-y01
-Title= Photoproduction
-YLabel=$\sigma_{\gamma\mathrm{p}}$
-XLabel=$\sqrt{s}$
+Title=Photoproduction
+YLabel=$\sigma_{\gamma\mathrm{p}}$ [pb]
+XLabel=$\sqrt{s}$ [GeV]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -9,35 +9,35 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d02-x01-y01
 Title= DIS
-YLabel=$\sigma_{\mathrm{DIS}}$
-XLabel=$\sqrt{s}$
+YLabel=$\sigma_{\mathrm{DIS}}$ [pb]
+XLabel=$\sqrt{s}$ [GeV]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d03-x01-y01
-Title= Photoproduction/DIS
+Title=Photoproduction
 YLabel=$\sigma_{\gamma\mathrm{p}}/\sigma_{\mathrm{DIS}}$
-XLabel=$\sqrt{s}$
+XLabel=$\sqrt{s}$ [GeV]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d04-x01-y01
-Title= DIS
+Title=DIS
 XLabel=$z_{\mathbb{P}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} z_{\mathbb{P}}$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} z_{\mathbb{P}}$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d05-x01-y01
-Title= DIS
+Title=DIS
 XLabel=$x_{\mathbb{P}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\mathbb{P}}$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\mathbb{P}}$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -46,7 +46,7 @@ RatioPlotYMax=2.2
 # BEGIN PLOT /H1_2015_I1343110/d06-x01-y01
 Title=DIS
 XLabel=$y$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} y$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} y$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -54,17 +54,18 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d07-x01-y01
 Title=DIS
-XLabel=$Q^2$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} Q^2$
+XLabel=$Q^2$ [GeV$^2$]
+YLabel=$\mathrm{d}\sigma / \mathrm{d} Q^2$ [pb/GeV$^2$]
 LogY=1
+LogX=1
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d08-x01-y01
 Title=DIS
-XLabel=$E_{\mathrm{T}}^{\mathrm{jet1}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} E_{\mathrm{T}}^{\mathrm{jet1}}$
+XLabel=$E_{\mathrm{T}}^{\mathrm{jet1}}$ [GeV]
+YLabel=$\mathrm{d}\sigma / \mathrm{d} E_{\mathrm{T}}^{\mathrm{jet1}}$  [pb/GeV]
 LogY=1
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -72,8 +73,8 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d09-x01-y01
 Title=DIS
-XLabel=$M_X$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} M_X$
+XLabel=$M_X$ [GeV$^2$]
+YLabel=$\mathrm{d}\sigma / \mathrm{d} M_X$ [pb/GeV$^2$]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -82,7 +83,7 @@ RatioPlotYMax=2.2
 # BEGIN PLOT /H1_2015_I1343110/d10-x01-y01
 Title=DIS
 XLabel=$|\Delta\eta|$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} |\Delta\eta|$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} |\Delta\eta|$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -90,26 +91,26 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d11-x01-y01
 Title=DIS
-XLabel=$<\eta>$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} <\eta>$
+XLabel=$\left<\eta\right>$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} \left<\eta\right>$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d12-x01-y01
-Title= Photoproduction
+Title=Photoproduction
 XLabel=$z_{\mathbb{P}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} z_{\mathbb{P}}$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} z_{\mathbb{P}}$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d13-x01-y01
-Title= Photoproduction
+Title=Photoproduction
 XLabel=$x_{\mathbb{P}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\mathbb{P}}$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\mathbb{P}}$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -118,7 +119,7 @@ RatioPlotYMax=2.2
 # BEGIN PLOT /H1_2015_I1343110/d14-x01-y01
 Title=Photoproduction
 XLabel=$y$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} y$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} y$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -127,7 +128,7 @@ RatioPlotYMax=2.2
 # BEGIN PLOT /H1_2015_I1343110/d15-x01-y01
 Title=Photoproduction
 XLabel=$x_{\gamma}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\gamma}$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} x_{\gamma}$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -135,8 +136,8 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d16-x01-y01
 Title=Photoproduction
-XLabel=$E_{\mathrm{T}}^{\mathrm{jet1}}$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} E_{\mathrm{T}}^{\mathrm{jet1}}$
+XLabel=$E_{\mathrm{T}}^{\mathrm{jet1}}$ [GeV]
+YLabel=$\mathrm{d}\sigma / \mathrm{d} E_{\mathrm{T}}^{\mathrm{jet1}}$ [pb/GeV]
 LogY=1
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -144,8 +145,8 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d17-x01-y01
 Title=Photoproduction
-XLabel=$M_X$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} M_X$
+XLabel=$M_X$ [GeV$^2$]
+YLabel=$\mathrm{d}\sigma / \mathrm{d} M_X$  [pb/GeV$^2$]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -154,7 +155,7 @@ RatioPlotYMax=2.2
 # BEGIN PLOT /H1_2015_I1343110/d18-x01-y01
 Title=Photoproduction
 XLabel=$|\Delta\eta|$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} |\Delta\eta|$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} |\Delta\eta|$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
@@ -162,15 +163,15 @@ RatioPlotYMax=2.2
 
 # BEGIN PLOT /H1_2015_I1343110/d19-x01-y01
 Title=Photoproduction
-XLabel=$<\eta>$
-YLabel=$\mathrm{d}\sigma / \mathrm{d} <\eta>$
+XLabel=$\left<\eta\right>$
+YLabel=$\mathrm{d}\sigma / \mathrm{d} \left<\eta\right>$ [pb]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d20-x01-y01
-Title= Photoproduction/DIS
+Title=Photoproduction/DIS
 YLabel=$\sigma_{\gamma\mathrm{p}}/\sigma_{\mathrm{DIS}}$
 XLabel=$|\Delta\eta|$
 LogY=0
@@ -179,7 +180,7 @@ RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d21-x01-y01
-Title= Photoproduction/DIS
+Title=Photoproduction/DIS
 YLabel=$\sigma_{\gamma\mathrm{p}}/\sigma_{\mathrm{DIS}}$
 XLabel=$y$
 LogY=0
@@ -188,7 +189,7 @@ RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d22-x01-y01
-Title= Photoproduction/DIS
+Title=Photoproduction/DIS
 YLabel=$\sigma_{\gamma\mathrm{p}}/\sigma_{\mathrm{DIS}}$
 XLabel=$z_{\mathbb{P}}$
 LogY=0
@@ -197,19 +198,10 @@ RatioPlotYMax=2.2
 # END PLOT
 
 # BEGIN PLOT /H1_2015_I1343110/d23-x01-y01
-Title= Photoproduction/DIS
+Title=Photoproduction/DIS
 YLabel=$\sigma_{\gamma\mathrm{p}}/\sigma_{\mathrm{DIS}}$
-XLabel=$E_T^{\mathrm{jet1}}$
+XLabel=$E_T^{\mathrm{jet1}}$ [GeV]
 LogY=0
 RatioPlotYMin=0.1
 RatioPlotYMax=2.2
 # END PLOT
-
-# ... add more histograms as you need them ...
-# BEGIN PLOT /H1_2015_I1343110/d01-x01-y01
-#Title=[Uncomment and insert title for histogram d01-x01-y01 here]
-#XLabel=[Uncomment and insert x-axis label for histogram d01-x01-y01 here]
-#YLabel=[Uncomment and insert y-axis label for histogram d01-x01-y01 here]
-# END PLOT
-
-# ... add more histograms as you need them ...
